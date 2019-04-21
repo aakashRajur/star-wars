403348c0b035f91a4b226579b7389308a0bff247
aakashRajur <aakashrajur16@gmail.com>
2019-04-21 21:28:01 +0530

    define enhanced logger package

---
+++ pkg/log/logger.go
@@ -0,0 +1,72 @@
+package log
+
+import (
+	"io"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Logger struct {
+	*logrus.Logger
+}
+
+func (logger *Logger) Info(args ...interface{}) {
+	logger.Logger.Infoln(args...)
+}
+
+func (logger *Logger) Trace(args ...interface{}) {
+	logger.Traceln(args...)
+}
+
+func (logger *Logger) Debug(args ...interface{}) {
+	logger.Debugln(args)
+}
+
+func (logger *Logger) Warn(args ...interface{}) {
+	logger.Warningln(args...)
+}
+
+func (logger *Logger) Error(args ...interface{}) {
+	logger.Logger.Errorln(args...)
+}
+
+func (logger *Logger) Fatal(args ...interface{}) {
+	logger.Logger.Fatalln(args...)
+}
+
+func (logger *Logger) InfoFields(fields map[string]interface{}, args ...interface{}) {
+	logger.WithFields(fields).Infoln(args...)
+}
+
+func (logger *Logger) TraceFields(fields map[string]interface{}, args ...interface{}) {
+	logger.WithFields(fields).Traceln(args...)
+}
+
+func (logger *Logger) DebugFields(fields map[string]interface{}, args ...interface{}) {
+	logger.WithFields(fields).Debugln(args...)
+}
+
+func (logger *Logger) WarnFields(fields map[string]interface{}, args ...interface{}) {
+	logger.WithFields(fields).Warnln(args...)
+}
+
+func (logger *Logger) ErrorFields(err error, fields map[string]interface{}, args ...interface{}) {
+	logger.WithError(err).WithFields(fields).Errorln(args...)
+}
+
+func (logger *Logger) FatalFields(fields map[string]interface{}, args ...interface{}) {
+	logger.WithFields(fields).Fatalln(args...)
+}
+
+func (logger *Logger) Out() io.Writer {
+	return logger.Logger.Out
+}
+
+func NewInstance(level logrus.Level, formatter logrus.Formatter) *Logger {
+	enhanced := logrus.New()
+	enhanced.SetLevel(level)
+	enhanced.SetFormatter(formatter)
+
+	logger := Logger{enhanced}
+	return &logger
+}
