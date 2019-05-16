cb9870e0b359e658ee6e1c529af47ed6597e7efe
aakashRajur <aakashrajur16@gmail.com>
2019-05-16 20:37:42 +0530

    add headers to logging

--- middleware/http/logger.go
+++ middleware/http/logger.go
@@ -10,17 +10,19 @@ func Logger(logger types.Logger) http.Middleware {
 		return func(response http.Response, request *http.Request) {
 			params := request.Context().Value(http.PARAMS).(map[string]interface{})
 			query := request.URL.Query()
+			headers := request.Header
 
 			logger.InfoFields(
 				map[string]interface{}{
-					"method":     request.Method,
-					"uri":        request.RequestURI,
-					"url":        request.URL.Path,
-					"params":     params,
-					"query":      query,
-					"remote":     request.RemoteAddr,
-					"referer":    request.Referer(),
-					"user-agent": request.UserAgent(),
+					`method`:     request.Method,
+					`uri`:        request.RequestURI,
+					`url`:        request.URL.Path,
+					`params`:     params,
+					`query`:      query,
+					`headers`:    headers,
+					`remote`:     request.RemoteAddr,
+					`referer`:    request.Referer(),
+					`user-agent`: request.UserAgent(),
 				},
 			)
 			next(response, request)
