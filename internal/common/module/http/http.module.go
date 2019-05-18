f04fb5784be8318b3113c0696bd05f88381d4076
aakashRajur <aakashrajur16@gmail.com>
2019-05-18 11:24:22 +0530

    add more header constants, don't set timeouts to support SSE

--- internal/common/module/http/http.module.go
+++ internal/common/module/http/http.module.go
@@ -1,10 +1,9 @@
 package http
 
 import (
-	"github.com/aakashRajur/star-wars/internal/common/module/router"
-	"time"
-
 	"go.uber.org/fx"
+
+	"github.com/aakashRajur/star-wars/internal/common/module/router"
 
 	"github.com/aakashRajur/star-wars/pkg/env"
 	"github.com/aakashRajur/star-wars/pkg/http"
@@ -15,15 +14,9 @@ func GetHttpServer(logger types.Logger, router *http.Router) *http.Server {
 	sslCert := env.GetString(`HTTP_SSL_CERT`)
 	sslKey := env.GetString(`HTTP_SSL_KEY`)
 	httpPort := ":" + env.GetString(`HTTP_PORT`)
-	timeoutConfig := http.Timeout{
-		Read:  time.Second * time.Duration(env.GetInt(`HTTP_READ_TIMEOUT`)),
-		Write: time.Second * time.Duration(env.GetInt(`HTTP_WRITE_TIMEOUT`)),
-		Idle:  time.Second * time.Duration(env.GetInt(`HTTP_IDLE_TIMEOUT`)),
-	}
 
 	serverConfig := http.ServerConfig{
 		Port:    httpPort,
-		Timeout: timeoutConfig,
 		SslCert: sslCert,
 		SslKey:  sslKey,
 		Logger:  logger,
--- pkg/http/headers.go
+++ pkg/http/headers.go
@@ -1,11 +1,16 @@
 package http
 
 const (
-	ContentType         = "Content-Type"
-	ContentTypeJSON     = "application/json"
-	ContentTypeText     = "application/text"
-	ContentLength       = `Content-Length`
-	ContentEncoding     = `Content-Encoding`
-	ContentEncodingGzip = `gzip`
-	XForwardedFor       = `X-Forwarded-For`
+	ContentType           = "Content-Type"
+	ContentTypeJSON       = "application/json"
+	ContentTypeText       = "application/text"
+	ContentTypeTextStream = `text/event-stream`
+	ContentLength         = `Content-Length`
+	ContentEncoding       = `Content-Encoding`
+	CacheControl          = `Cache-Control`
+	CacheControlNoCache   = `no-cache`
+	Connection            = `Connection`
+	ConnectionKeepAlive   = `keep-alive`
+	ContentEncodingGzip   = `gzip`
+	XForwardedFor         = `X-Forwarded-For`
 )
