c0ee1a58828ff56b27e3e26d5f2faf9b14c96284
aakashRajur <aakashrajur16@gmail.com>
2019-05-19 13:01:31 +0530

    print verbose function name

--- internal/common/module/instrumentation/instrumentation.module.go
+++ internal/common/module/instrumentation/instrumentation.module.go
@@ -1,7 +1,6 @@
 package instrumentation
 
 import (
-	"regexp"
 	"runtime"
 	"time"
 
@@ -16,17 +15,12 @@ func GetExecutionTracer(logger types.Logger) types.TimeTracker {
 
 		// Skip this function, and fetch the PC and file for its parent.
 		pc, _, _, _ := runtime.Caller(1)
-
 		// Retrieve a function object this functions parent.
 		funcObj := runtime.FuncForPC(pc)
 
-		// Regex to extract just the function name (and not the common path).
-		runtimeFunc := regexp.MustCompile(`^.*\.(.*)$`)
-		name := runtimeFunc.ReplaceAllString(funcObj.Name(), "$1")
-
 		logger.InfoFields(
 			map[string]interface{}{
-				`function`: name,
+				`function`: funcObj.Name(),
 				`elapsed`:  elapsed,
 			},
 		)
